1a7d8a366aa7d720eef38483098935b2765f408f
SteveWhitley <sgwhitley@aggies.ncat.edu>
2023-04-08 22:29:33 -0400

    WIP Base Game
    WIP

---
+++ Marble Mania/Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public class Movement : MonoBehaviour
+// {
+//     public Rigidbody marble;
+//     public float speed = 5.0f;
+//     private float horizontalInput;
+//     private float forwardInput;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         float mosueX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+//         float mosueY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+//         yRotation += mouseX;
+//         xRotation -= MouseY;
+//         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+//         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        //marble.isKinematic = true;
+//     }
+// }
